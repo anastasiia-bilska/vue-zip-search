bcdaddcafac7fbe8e64bd482bdebd74dcd01dac3
Anastasiia Bilska <nastya.bilskaya@gmail.com>
2023-03-24 16:24:42 +0200

    minor fix

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
-import HomePage from '../components/pages/HomePage.vue';
-import ZIPSearchButton from '../components/pages/ZIPSearchPage.vue';
+import { createRouter, createWebHashHistory } from 'vue-router'
+import HomePage from '../components/pages/HomePage.vue'
+import ZIPSearchButton from '../components/pages/ZIPSearchPage.vue'
 
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
@@ -8,14 +8,14 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomePage,
+      component: HomePage
     },
     {
       path: '/search',
       name: 'search',
-      component: ZIPSearchButton,
+      component: ZIPSearchButton
     }
-  ],
+  ]
 })
 
 export default router
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 // eslint-disable-next-line no-undef
 module.exports = {
-  content: ["./src/**/*.{vue,ts,tsx,jsx,js}", "html"],
+  content: ['./src/**/*.{vue,ts,tsx,jsx,js}', 'html'],
   theme: {
     extend: {
-    },
+    }
   },
-  plugins: [],
+  plugins: []
 }
